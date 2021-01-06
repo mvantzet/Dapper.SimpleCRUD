5197c20238f96e9c951ec03758921fd5946c8078
mvantzet <mark@radialsg.com>
2021-01-06 10:40:49 +0100

    Added a CachingNameResolver, still a work in progress

---
+++ Dapper.SimpleCRUD/SimpleCRUDMapper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Dapper
+{
+    public static partial class SimpleCRUD
+    {
+        public class CachingNameResolver : SimpleCRUD.IColumnNameResolver, SimpleCRUD.ITableNameResolver
+        {
+            private class Mapping
+            {
+                public string TableName { get; set; }
+
+                public Dictionary<PropertyInfo, string> ColumnName { get; } = new Dictionary<PropertyInfo, string>();
+            }
+
+            private readonly ConcurrentDictionary<Type, Mapping> _mappings = new ConcurrentDictionary<Type, Mapping>();
+            private readonly ColumnNameResolver _columnNameResolver;
+            private readonly TableNameResolver _tableNameResolver;
+
+            public CachingNameResolver(INameFormatter nameFormatter)
+            {
+                _columnNameResolver = new ColumnNameResolver(nameFormatter);
+                _tableNameResolver = new TableNameResolver(nameFormatter);
+            }
+
+            private Mapping Get(Type type)
+            {
+                lock (_mappings)
+                {
+                    if (_mappings.TryGetValue(type, out var mapping))
+                    {
+                        return mapping;
+                    }
+
+                    mapping = new Mapping
+                    {
+                        TableName = _tableNameResolver.ResolveTableName(type)
+                    };
+                    foreach (var p in type.GetProperties())
+                    {
+                        mapping.ColumnName.Add(p, _columnNameResolver.ResolveColumnName(p));
+                    }
+
+                    _mappings.AddOrUpdate(type, t => mapping, (t, m2) => mapping);
+
+                    return mapping;
+                }
+            }
+
+            public string ResolveColumnName(PropertyInfo propertyInfo)
+            {
+                return Get(propertyInfo.DeclaringType).ColumnName[propertyInfo];
+            }
+
+            public virtual string ResolveTableName(Type type)
+            {
+                return Get(type).TableName;
+            }
+        }
+    }
+}
